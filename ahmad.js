df69a199bb4cf0ef75afa81a0090b91f44840a88
Ahmad Moussalli <ahmadmoussalli@lucidmotors.com>
2020-02-26 18:55:19 -0800

    Added Custom API, Modefied app.js

---
+++ ahmad.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const ahmadsFunction = (req,res) => {
+  axios({
+    "method":"GET",
+    "url":"https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-12-01",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    "x-rapidapi-key":"b4d2b2e30fmshce06cba50a79ad8p1327fcjsn8932f92991ac"
+    }
+    })
+    .then((response)=>{
+      console.log(response)
+      res.send({
+        status: "OK",
+        date: new Date(),
+        //response: 'the response is '+ response['Quotes'],
+      })
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+}
+
+module.exports = ahmadsFunction;
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const lesliesFunction = require('./leslie.js')
 const abista7Function = require('./abista7.js')
+const ahmadsFunction = require('./ahmad.js')
 const app = express();
 
 app.listen(3000)
 
+app.get('/skyscanner', ahmadsFunction)
 app.get('/xposed', lesliesFunction)
 app.get('/abista7', abista7Function)
 
 app.get('*', (req, res) => {
-    res.send('Hello World!')
+    res.send('Express Masters HW1')
 })
