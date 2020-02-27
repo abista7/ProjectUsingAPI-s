5ec69434c1188e18eda73bf264e997319eb6decf
Leslie Zhou <zzhou2@mail.sfsu.edu>
2020-02-27 00:11:12 -0800

    invalid param message

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const errorMessage = require('./error.js')
 const lesliesFunction = require('./leslie.js')
 const abista7Function = require('./abista7.js')
 const app = express();
 
-app.listen(3001)
+app.listen(3000)
 
 app.get('/xposed', lesliesFunction)
 app.get('/abista7', abista7Function)
 
 app.get('*', (req, res) => {
-    res.send({
-        status: 'ERROR',
-        date: new Date(),
-        param: req.query,
-        response: 'Invalid query param',
-    })
+    res.send(errorMessage(req))
 })
