94839e244b6b44cb01dd15c9b8734938b4e3e2e0
sanjay417 <miranisanjay417@gmail.com>
2020-02-28 19:20:50 -0800

    updated app.js, grading.csv and added sanjay417.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const errorMessage = require('./error.js')
 const lesliesFunction = require('./leslie.js')
 const abista7Function = require('./abista7.js')
+const sanjay417Function = require('./sanjay417.js')
 const app = express();
 
 app.listen(3000)
 
-app.get('/xposed', lesliesFunction)
-app.get('/abista7', abista7Function)
+app.get('/app/xposed', lesliesFunction)
+app.get('/app/abista7', abista7Function)
+app.get('/app/sanjay417', sanjay417Function)
 
 app.get('*', (req, res) => {
     res.send(errorMessage(req))
