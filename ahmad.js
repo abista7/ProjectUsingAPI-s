d54eec838184999ee9b503a7eaa13779863a0afd
Ahmad Moussalli <ahmoussalli@gmail.com>
2020-02-29 20:44:45 -0800

    Added Pram to URL

--- ahmad.js
+++ ahmad.js
@@ -1,29 +1,33 @@
 const axios = require("axios");
+//const errorMessage = require('./error.js')
 const ahmadsFunction = (req, res) => {
+    // if (req.query.city == null || req.query.city == '') {
+    //     res.send(errorMessage(req))
+    //     return
+    // }
+    const destinationCity = req.query.city;
     axios({
             "method": "GET",
-            "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2020-12-01",
+            "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/"+destinationCity+"/2020-12-01",
             "headers": {
                 "content-type": "application/octet-stream",
                 "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
                 "x-rapidapi-key": "b4d2b2e30fmshce06cba50a79ad8p1327fcjsn8932f92991ac"
             }
         })
-        .then((response) => {
-            //console.log(response)
-            let quotes = ''
-            response.data.Quotes.forEach(element => {
-                quotes = quotes + '\n' + JSON.stringify(element)
-            });
-            res.send({
-                status: "OK",
-                date: new Date(),
-                response: "the response is" + quotes,
-            })
+    .then((response) => {
+        let quotes = ''
+        response.data.Quotes.forEach(element => {
+            quotes = quotes + JSON.stringify(element.MinPrice);
+        });
+        res.send({
+            status: "OK",
+            date: new Date(),
+            response: "The lowest flight price is: " + quotes +" USD",
         })
-        .catch((error) => {
-            console.log(error)
-        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 }
-
 module.exports = ahmadsFunction;
